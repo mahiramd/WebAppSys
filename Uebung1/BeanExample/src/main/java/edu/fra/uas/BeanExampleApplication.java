23dce9b7daa1ecc107aea4eb1f59f43b086adc8d
mahiramd <mahirahmad112@gmail.com>
2025-10-20 13:01:02 +0200

    Übung 1

--- Uebung1/BeanExample/src/main/java/edu/fra/uas/BeanExampleApplication.java
+++ Uebung1/BeanExample/src/main/java/edu/fra/uas/BeanExampleApplication.java
@@ -1,12 +1,18 @@
 package edu.fra.uas;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import edu.fra.uas.service.MessageService;
+
 @SpringBootApplication
 public class BeanExampleApplication {
+
+	@Autowired
+	private MessageService messageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeanExampleApplication.class, args);
@@ -15,9 +21,14 @@ public class BeanExampleApplication {
 	@Bean
 	CommandLineRunner init() {
 		CommandLineRunner action = new CommandLineRunner() {
+
 			@Override
 			public void run(String... args) throws Exception {
 				System.out.println("Hello World!");
+				System.out.println(messageService.getMessage());
+				messageService.setMessage("--> HHHOHHH <--");
+				System.out.println(messageService.getMessage());
+
 			}
 		};
 		return action;
